c67f712ca58e8a83020b765017d53236bfd4bd3d
rifqi856 <rifqithufail1208@gmail.com>
2025-04-30 20:13:07 +0700

    perpustakaan

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build', // pastikan output diarahkan ke folder yang benar
+        manifest: true, // pastikan manifest dihasilkan
+    },
     plugins: [
         laravel({
             input: [
